d762301fa53676d922089635a9331c81b3ba6785
adamdev <hmullinsk@hotmail.com>
2023-05-15 02:08:56 -0400

    cleared out the project

--- BuildInfo.cs
+++ BuildInfo.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace bonelab_template
+﻿namespace bonelab_template
 {
     internal partial class Main
     {
